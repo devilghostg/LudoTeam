7aed90d363a22de059c4f3ee4d6173f940a9e990
devilghostg <mister.devil6@gmail.com>
2025-01-17 15:43:12 +0400

    bug: test unitaire gamecontroller / status : 200 so max_players= interger, no string

--- tests/Controller/GameControllerTest.php
+++ tests/Controller/GameControllerTest.php
@@ -9,8 +9,18 @@ final class GameControllerTest extends WebTestCase
     public function testIndex(): void
     {
         $client = static::createClient();
-        $client->request('GET', '/game');
-
+        
+        // Test basic
+        $crawler = $client->request('GET', '/game');
         self::assertResponseIsSuccessful();
+        
+        // Test  number 
+        $crawler = $client->request('GET', '/game?availability=20');
+        self::assertResponseIsSuccessful();
+        
+        // Test sur l'inclusion d'un nom dans le max players
+        $crawler = $client->request('GET', '/game?max_players=test');
+        self::assertResponseIsSuccessful();
+        
     }
 }
